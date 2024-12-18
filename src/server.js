af8ef40de1f2179d836e6a08952d8ae1d7d2b68b
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-12-18 17:10:28 +0600

    explore get method

--- src/server.js
+++ src/server.js
@@ -1,6 +1,18 @@
 const express =require("express");
 const app= express();
 
+app.get("/",(req,res)=>{
+    res.status(200).send({
+        message: "welcome to our server"
+    })
+})
+
+app.get("/products", (req,res)=>{
+    res.status(200).send({
+        message: "products are returned"
+    })
+})
+
 app.listen(3001, ()=>{
     console.log(`server is running at http://localhost:3001`)
 })
